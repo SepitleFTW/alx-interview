b3d62e49a57ae71a35bab450919128734cb09fe4
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-07-11 21:51:52 +0200

    ...

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,17 +5,24 @@ minimum operations
 
 
 def minOperations(n):
-    if n <= 1:
+    '''min operations
+    '''
+    if not isinstance(n, int):
         return 0
-
-    operations = 0
-    divisor = 2
-
-    while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
-
-    return operations
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    while done < n:
+        if clipboard == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif clipboard > 0:
+            done += clipboard
+            ops_count += 1
+    return ops_count
 
