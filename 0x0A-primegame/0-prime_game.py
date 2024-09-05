0630b9f4e9c79a5091109d7afa90d676cfbb6640
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-09-05 21:09:39 +0200

    chatgpt helped with outline for solution

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""prime game final solution
+"""
+
+
+def isWinner(x, nums):
+    """finding the winner of the prime game
+    """
+    if x < 1 or not nums:
+        return None
+    maria_wins, ben_wins = 0, 0
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        ben_wins += primes_count % 2 == 0
+        maria_wins += primes_count % 2 == 1
+    if maria_wins == ben_wins:
+        return None
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
